d521130ee2c74ecff26530e71f4bc738d5466b1e
HaireJason-FS <cjhaire@student.fullsail.edu>
2025-10-29 13:27:26 -0400

    made changes in vscode

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 // Imports your SCSS stylesheet
 import './styles/index.scss';
 console.log('Application started version 2.0');
+
+// Application logic can be added here
+// For example, initializing UI components or handling user interactions
