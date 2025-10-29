3e382f0999717952305741088de10d829722396d
HaireJason-FS <cjhaire@student.fullsail.edu>
2025-10-29 13:45:16 -0400

    removed files

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 // Imports your SCSS stylesheet
 import './styles/index.scss';
+//import car dataset package
+import carData from './car-dataset/car-dataset.json';
 console.log('Application started version 2.0');
 
-// Application logic can be added here
-// For example, initializing UI components or handling user interactions
+//We are building a used car finder program. The user will be able to search for used cars based on various criteria (such as year, make, and model). The user needs to be able to see what section they're on by the others being grayed out(displayed by a gray text color instead of blue(showing the user they have completed the section or need to complete). We will use the car-dataset npm package to get the car data we need and to show the user the information inside the dropdowns.
+
+
+
